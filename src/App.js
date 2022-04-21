79c3e13bf92dfc1ac4f8b0b477dbdb2afcc88804
Emanuelhitallo <hitallosoares1@gmail.com>
2022-04-21 11:23:52 -0300

    Corrigindo erros

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import './App.css';
 
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [apiResponse, setApiResponse] = useState('');
 
-  const [isEmailValid, setIsEmailValid] = useState(false);
-  const [EmailErroMessage, setEmailErroMessage] = useState('');
   const [isValidPassword, setIsValidPassword] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  
   const [PasswordErroMessage, setPasswordErroMessage] = useState('');
-  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const [EmailErroMessage, setEmailErroMessage] = useState('');
 
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const validatePassword = (event) => {
     const password = event.target.value;
+    setPassword(event.target.value)
 
     if (password.length >= 6) {
       setIsValidPassword(true);
       setPasswordErroMessage("Senha valida!")
+
+
     } else {
       setIsValidPassword(false)
       setPasswordErroMessage("A senha deve ter no minimo 6 caracteres")
@@ -25,28 +32,72 @@ function App() {
 
   const validateEmail = (event) => {
     const email = event.target.value;
+    setEmail(event.target.value);
 
     if (emailRegex.test(email)) {
       setIsEmailValid(true);
-      setEmailErroMessage('Email Valido!');
+      setEmailErroMessage('Email valido!');
+
     } else {
       setIsEmailValid(false);
       setEmailErroMessage('Digite um email Valido!');
     }
-
   };
 
-  return (
-    <div id="login-container">
-      <h1>Entrar</h1>
 
-      <form >
+  const disabledButton = () => {
+
+    if (isEmailValid && isValidPassword) {
+      return false;
+
+    } else {
+      return true;
+    }
+  }
+  const submitForm = (event) => {
+    event.preventDefault();
+   
+    fetch('http://localhost:3001/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: email, password: password})
+
+    })
+    .then((res) => res.json())
+    .then((json) => setApiResponse(json))
+
+    .catch(e => console.log(" erro!!",)) 
+    
+var res = window.document.getElementById('res')
+  var texto = JSON.stringify(apiResponse)
+  var texto2 = JSON.parse(texto)
+ if(texto2.message){
+  res.innerHTML = texto2.message
+ } else {
+  res.innerHTML = texto2.error
+
+ }
+
+  }
+
+  
+  
+
+  return (
+  
+<div id="login-container">    
+      <h1>Entrar</h1>
+     
+      <form onSubmit={submitForm}>
         <label for="email">Email</label>
         <input
           type="email"
           name='email'
           id='email'
           placeholder='Email...'
+          value={email}
           onChange={validateEmail}>
         </input>
         <div className={`message ${isEmailValid ? 'success' : 'error'}`}>
@@ -57,7 +108,9 @@ function App() {
           type="password"
           name="password"
           placeholder='Senha'
-          onChange={validatePassword}>
+          value={password}
+          onChange={validatePassword}
+        >
         </input>
         <div className={`message ${isValidPassword ? 'success' : 'error'}`}>
           {PasswordErroMessage}
@@ -65,13 +118,19 @@ function App() {
 
         <a href='#' id='forgot-pass'>Esqueceu a senha?</a>
         <input type="submit" id='botao-cadastrar-se' value="Cadastre-se"></input>
-        <input type="submit" id='botao-entrar' value="Entrar"></input>
+        <input
+          type="submit"
+          id='botao-entrar'
+          value="Entrar"
+          disabled={disabledButton()}
+        ></input>
       </form>
-      
+      <div id="res">Resultado </div>
     </div>
-    
-    
+
+
   );
+
 }
 
 export default App;
